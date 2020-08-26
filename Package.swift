e6239e2185a315bc2181b026c2cfb00fd34a62ba
Mhd Hejazi <mohammadhi@gmail.com>
2020-08-27 01:18:53 +0300

    Rename library target.

--- Package.swift
+++ Package.swift
@@ -4,9 +4,9 @@ import PackageDescription
 let package = Package(
     name: "DiffMatchPatch",
     products: [
-        .library(name: "DiffMatchPatch", targets: ["diff_match_patch"]),
+        .library(name: "DiffMatchPatch", targets: ["DiffMatchPatch"]),
     ],
     targets: [
-        .target(name: "diff_match_patch", dependencies: []),
+        .target(name: "DiffMatchPatch", dependencies: []),
     ]
 )
