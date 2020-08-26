bf47a562f865243e3cdb54d97310276c1326440f
Mhd Hejazi <mohammadhi@gmail.com>
2020-08-27 00:48:29 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,12 @@
+// swift-tools-version:5.2
+import PackageDescription
+
 import PackageDescription
 
 let package = Package(
     name: "DiffMatchPatch",
-    targets: [Target(name: "DiffMatchPatch", dependencies:["diff_match_patch"])]
+	targets: [
+		.target(name: "diff_match_patch", dependencies: []),
+		.target(name: "DiffMatchPatch", dependencies:["diff_match_patch"])
+	]
 )
